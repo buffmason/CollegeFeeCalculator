4adda29341cccbc08eb173e9bde389018a1c8f14
buffmason <buffmason@gmail.com>
2024-05-05 23:33:04 -0400

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CollegeFeeCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.cmbMajor = new System.Windows.Forms.ComboBox();
+            this.lblMajor = new System.Windows.Forms.Label();
+            this.chkResidentStatus = new System.Windows.Forms.CheckBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblCalculate = new System.Windows.Forms.Label();
+            this.txtCalculate = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(91, 34);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(110, 35);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(217, 34);
+            this.txtFirstName.Multiline = true;
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(102, 33);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(91, 98);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(110, 35);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(217, 100);
+            this.txtLastName.Multiline = true;
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(102, 33);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // lblAge
+            // 
+            this.lblAge.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(113, 166);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(110, 35);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Age";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(217, 168);
+            this.txtAge.Multiline = true;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(102, 33);
+            this.txtAge.TabIndex = 5;
+            // 
+            // cmbMajor
+            // 
+            this.cmbMajor.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMajor.FormattingEnabled = true;
+            this.cmbMajor.Items.AddRange(new object[] {
+            "Computer Science",
+            "Business Administration",
+            "Arts and Humanities",
+            "Sciences"});
+            this.cmbMajor.Location = new System.Drawing.Point(522, 39);
+            this.cmbMajor.Name = "cmbMajor";
+            this.cmbMajor.Size = new System.Drawing.Size(194, 33);
+            this.cmbMajor.TabIndex = 6;
+            // 
+            // lblMajor
+            // 
+            this.lblMajor.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMajor.Location = new System.Drawing.Point(385, 39);
+            this.lblMajor.Name = "lblMajor";
+            this.lblMajor.Size = new System.Drawing.Size(110, 35);
+            this.lblMajor.TabIndex = 7;
+            this.lblMajor.Text = "Select Major";
+            // 
+            // chkResidentStatus
+            // 
+            this.chkResidentStatus.AutoSize = true;
+            this.chkResidentStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkResidentStatus.Location = new System.Drawing.Point(573, 104);
+            this.chkResidentStatus.Name = "chkResidentStatus";
+            this.chkResidentStatus.Size = new System.Drawing.Size(99, 29);
+            this.chkResidentStatus.TabIndex = 8;
+            this.chkResidentStatus.Text = "In-State";
+            this.chkResidentStatus.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(544, 168);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(159, 58);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "Calculate Fee";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblCalculate
+            // 
+            this.lblCalculate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalculate.Location = new System.Drawing.Point(212, 275);
+            this.lblCalculate.Name = "lblCalculate";
+            this.lblCalculate.Size = new System.Drawing.Size(158, 35);
+            this.lblCalculate.TabIndex = 10;
+            this.lblCalculate.Text = "Calculated Fee";
+            // 
+            // txtCalculate
+            // 
+            this.txtCalculate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCalculate.Location = new System.Drawing.Point(435, 272);
+            this.txtCalculate.Multiline = true;
+            this.txtCalculate.Name = "txtCalculate";
+            this.txtCalculate.Size = new System.Drawing.Size(102, 33);
+            this.txtCalculate.TabIndex = 11;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(108, 357);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(159, 58);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear Form";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(497, 357);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(159, 58);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtCalculate);
+            this.Controls.Add(this.lblCalculate);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.chkResidentStatus);
+            this.Controls.Add(this.lblMajor);
+            this.Controls.Add(this.cmbMajor);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "MainForm";
+            this.Text = "College Fee Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ComboBox cmbMajor;
+        private System.Windows.Forms.Label lblMajor;
+        private System.Windows.Forms.CheckBox chkResidentStatus;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblCalculate;
+        private System.Windows.Forms.TextBox txtCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollegeFeeCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Naming each of the variables and giving them either a string or double value
+            string firstName, lastName, age;
+            double price, majorPrice, totalPrice;
+
+            // Storing values from textboxes while also trimming any leading/following whitespaces
+            firstName = txtFirstName.Text.Trim();
+            lastName = txtLastName.Text.Trim();
+            age = txtAge.Text.Trim();
+
+            // Show error message if no First name is entered
+            if (firstName.Length == 0)
+            {
+                MessageBox.Show("Please enter a valid First name");
+                txtFirstName.Focus();
+                txtFirstName.Clear();
+                return;
+            }
+
+            // Show error message if no last Name is entered
+            if (lastName.Length == 0) 
+            {
+                MessageBox.Show("Please enter a valid Last Name");
+                txtLastName.Focus();
+                txtLastName.Clear();
+                return;
+            }
+
+            // Show error message if no Age is entered
+            if (age.Length == 0)
+            {
+                MessageBox.Show("Please enter a valid Age");
+                txtAge.Focus();
+                txtAge.Clear();
+                return;
+            }
+
+            // Show error message if no major is chosen
+            if (cmbMajor.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please enter your declared major");
+                cmbMajor.Focus();
+                cmbMajor.SelectedIndex = -1;
+                return;
+            }
+
+            // Makes price $10,000 if the in-state box is selected, otherwise price is $15,000
+            if (chkResidentStatus.Checked)
+            {
+                price = 10000;
+            }
+            else
+            {
+                price = 15000;
+            }
+
+            // Adds a price to the combo box selection that the user chooses
+            if (cmbMajor.SelectedItem == "Computer Science")
+            {
+                majorPrice = 2000;
+            }
+            else if (cmbMajor.SelectedItem == "Business Administration")
+            {
+                majorPrice = 1500;
+            }
+            else if (cmbMajor.SelectedItem == "Arts and Humanities")
+            {
+                majorPrice = 1000;
+            }
+            else if (cmbMajor.SelectedItem == "Sciences")
+            {
+                majorPrice = 1800;
+            }
+            else
+            {
+                majorPrice = 0;
+            }
+
+            // Creates new variable called totalPrice that is the majorPrice + price (of in-state or out of state)
+            totalPrice = price + majorPrice;
+
+            // Displays the total price as a currency value
+            txtCalculate.Text = totalPrice.ToString(("C")); 
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clearing all fields when "clear" button is hit
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtAge.Clear();
+            txtCalculate.Clear();
+            cmbMajor.SelectedIndex = -1;
+            chkResidentStatus.Checked = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Closing the application
+            this.Close();
+        }
+    }
+}
